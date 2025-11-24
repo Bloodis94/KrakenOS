cbf1f08c9f39bc214a0dcfe43ec09154eb9af308
Alessandro Pirastru <alessandro.pirastru.94@gmail.com>
2025-11-24 03:48:34 +0100

    moved flatpaks to system

--- build_files/customization_packages/flatpak.sh
+++ build_files/customization_packages/flatpak.sh
@@ -4,22 +4,22 @@ set -ouex pipefail
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install -y flathub com.github.marhkb.Pods
-flatpak install -y flathub io.github.flattool.Warehouse
-flatpak install -y flathub com.github.tchx84.Flatseal
-flatpak install -y flathub com.dec05eba.gpu_screen_recorder
-flatpak install -y flathub io.github.ilya_zlobintsev.LACT
-flatpak install -y flathub io.github.kolunmi.Bazaar
-flatpak install -y flathub org.onlyoffice.desktopeditors
-flatpak install -y flathub com.slack.Slack
-flatpak install -y flathub org.gnome.Totem
-flatpak install -y flathub org.kde.kdenlive
-flatpak install -y flathub org.kde.okular
-flatpak install -y flathub org.kde.dolphin
-flatpak install -y flathub org.kde.filelight
-flatpak install -y flathub org.fedoraproject.MediaWriter
-flatpak install -y flathub com.nextcloud.desktopclient.nextcloud
-flatpak install -y flathub md.obsidian.Obsidian
-flatpak install -y flathub rest.insomnia.Insomnia
-flatpak install -y flathub org.kde.kate
-flatpak install -y flathub dev.deedles.Trayscale
+flatpak install -y --system flathub com.github.marhkb.Pods
+flatpak install -y --system flathub io.github.flattool.Warehouse
+flatpak install -y --system flathub com.github.tchx84.Flatseal
+flatpak install -y --system flathub com.dec05eba.gpu_screen_recorder
+flatpak install -y --system flathub io.github.ilya_zlobintsev.LACT
+flatpak install -y --system flathub io.github.kolunmi.Bazaar
+flatpak install -y --system flathub org.onlyoffice.desktopeditors
+flatpak install -y --system flathub com.slack.Slack
+flatpak install -y --system flathub org.gnome.Totem
+flatpak install -y --system flathub org.kde.kdenlive
+flatpak install -y --system flathub org.kde.okular
+flatpak install -y --system flathub org.kde.dolphin
+flatpak install -y --system flathub org.kde.filelight
+flatpak install -y --system flathub org.fedoraproject.MediaWriter
+flatpak install -y --system flathub com.nextcloud.desktopclient.nextcloud
+flatpak install -y --system flathub md.obsidian.Obsidian
+flatpak install -y --system flathub rest.insomnia.Insomnia
+flatpak install -y --system flathub org.kde.kate
+flatpak install -y --system flathub dev.deedles.Trayscale
